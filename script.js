2003ab199cd14fc55da53df55961562c0f2929d4
AprendIgor <107777408+IgorVsFeijo@users.noreply.github.com>
2023-10-25 21:39:25 -0300

    dica da jubs

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
     const edw24ard5Cul = "https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaAgh9C02T_kyXMtAn0UbKvcT46kargAF21C7-gCDe_HeGSUrxSYDK9ySll82oEPrifylm4M89dGKGwKrB8vlwJQgjXazw=w1366-h620";
     const b2ella4Swan6 = bell42aVision("tffbe://xt3.saasxqgeqdoazfqzf.oay/g/0/pduhq-huqiqd/MW7mBmNdkKa25YGqdIXLzrRjDvZbih4kAvRhbwej0KkPkW0DeFiUCt3ZTwKGQJ_jC7x715bQlal0Nt5qv9BHxTN_7LwOnpV3Bi=i1366-t620", 12);
-    
+
     const werewolfTrans92form = 15000;
 
     function bell42aVision(v5ampireTxt, ja34cobShift) {
@@ -40,7 +40,7 @@
     setInterval(vol76turiA, 1000);
 
     const ja45cobW6olf = document.getElementById("mainImage");
-    
+
     function cha42ngeS() {
         if (ja45cobW6olf.src === edw24ard5Cul) {
             ja45cobW6olf.src = b2ella4Swan6;
@@ -53,9 +53,6 @@
 
     function witn34essCh() {
 
-        console.log(b2ella4Swan6);
-        console.log(edw24ard5Cul);
-    
         const curr5Moon = new Date();
         if (curr5Moon.getHours() === curr5Moon.getMinutes()) {
             setTimeout(() => {
@@ -70,18 +67,18 @@
             }, werewolfTrans92form - 50);
         }
 
-        if(curr5Moon.getMinutes()%6 == 0){
+        if (curr5Moon.getMinutes() % 6 == 0) {
 
             document.getElementById("dica").style.display = "block";
 
             document.getElementById("dica").textContent = bell42aVision("Tg jgtçg jgy xagy, utjk uy tusky zkiks zxgsgy, g jgsg Sgxog jk Raxjky Tatky sgxig vxkyktçg. Tu hgoxxu wak kiug xoyuy k rgfkx, utjk ktzxk gargy yaxmk g vgayg — ykxog nuxg jk as rgtink? Ks skou à bgyzojãu bkxjk jk asg iojgjk kyiutjojg, asg pubks gxzoyzg jk ighkruy utjargjuy, Parogtg, yk jkyzgig. Kshuxg saozuy zxgtyozks vux gro, vuaiuy vkxikhks gwakrk tusk kshrksázoiu, wak zgshés kbuig g osgmks jk waks maog u igsotnu.", 6);
         }
 
-        else{
+        else {
             document.getElementById("dica").textContent = "";
             document.getElementById("dica").style.display = "none";
         }
-    
+
         if (newMo76onPh) {
             clearInterval(newMo76onPh);
         }
@@ -91,21 +88,77 @@
     events = ["visibilitychange", "DOMContentLoaded"];
 
     events.forEach(event => {
-        document.addEventListener(event, function() {
+        document.addEventListener(event, function () {
 
-            console.log(!document.hidden);
-        
             if (!document.hidden) {
                 ja45cobW6olf.src = edw24ard5Cul;
                 witn34essCh;
-                iniciar  = setInterval(witn34essCh, werewolfTrans92form);
+                iniciar = setInterval(witn34essCh, werewolfTrans92form);
             }
-            else{
+            else {
                 clearTimeout(paulina);
                 clearInterval(iniciar);
             }
-        
+
         });
     });
+
+    aparecerBotao = setInterval(() => {
+        horarioatual = new Date();
+
+        if (horarioatual.getSeconds() == (60-19)) {
+            botao.style.display = "block";
+            botao.style.top = (Math.round(Math.random()*100)) + "%";
+            botao.style.right = (Math.round(Math.random()*100)) + "%";
+        }
+
+        else{
+            botao.style.display = "none";
+        }
+
+    }, 500)
+
+    botao.addEventListener("mousedown", () => {
+
+        acionar = true;
+
+        document.addEventListener("keydown", (evento) => {
+            
+            posicaoatual = audio1.currentTime;
+
+            if(audio1.paused && acionar){
+                audio1.play();
+            }
+
+        })
+
+        document.addEventListener("keyup", () => {
+
+            setTimeout(()=>{       
+                if(acionar) audio1.pause();
+            }, 400)
+
+        })
+
+        entrada = window.prompt("?");
+
+        if(entrada.toUpperCase() == 'SANS'){
+            document.getElementById("espacovazio").innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/Dk-JzU8c60w?si=BADOYHSQeXgWUma3&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+
+            setTimeout(()=>{
+                document.getElementById("espacovazio").innerHTML = "";
+            }, 30000);
+        }
+
+        setTimeout(()=>{
+            acionar = false;
+            audio1.pause();
+        },5000);
+
+    })
+    audio1 = new Audio("audio1.mp3");
+    audio1.loop = 1;
+    botao = document.getElementById("botao");
+    botao.style.display = "none";
 
 })();
